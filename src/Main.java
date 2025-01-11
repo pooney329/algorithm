f833d1d73d6b78691543fd04cf68abc4b8a10a03
janghyeonwoo <blue672@naver.com>
2025-01-11 23:44:02 +0900

    Problem11050 시간 초과

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
 public class Main {
 
-    public static class Problem27433 {
-
+    public static class Problem11050 {
+        public static int number = 0;
         public void solution() throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int N = Integer.parseInt(br.readLine());
-            System.out.println(recursive(N));
+            StringTokenizer st = null;
+
+            int count = Integer.parseInt(br.readLine());
+            StringBuilder sb = new StringBuilder();
+            while(count --> 0){
+                number = 0;
+                st = new StringTokenizer(br.readLine());
+                final int N = Integer.parseInt(st.nextToken());
+                final int K = Integer.parseInt(st.nextToken());
+                recursive(N,K);
+                sb.append(number).append("\n");
+            }
+            System.out.println(sb);
         }
 
-        public long recursive(long num){
-            if(num <= 1) return 1;
-            return num * recursive(num-1);
+        public void recursive(int n , int k){
+            if(n <= 0 || n == k )  {
+                number = number + 1;
+                return;
+            }
+
+            for(int i = 1; i <= k; i++){
+                recursive(n-1,k-i);
+            }
         }
     }
 
 
     public static void main(String[] args) throws IOException {
-        Problem27433 problem = new Problem27433();
+        Problem11050 problem = new Problem11050();
         problem.solution();
     }
 }
