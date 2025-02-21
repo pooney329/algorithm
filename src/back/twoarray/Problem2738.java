bb36d8fac1baeb62bc21806f3570212c6606d6db
janghyeonwoo <blue672@naver.com>
2025-02-21 22:36:28 +0900

    Problem2738 add

--- src/back/twoarray/Problem2738.java
+++ src/back/twoarray/Problem2738.java
@@ -1,11 +1,15 @@
+package back.twoarray;
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
-public class Main {
-
+/**
+ * {@link https://www.acmicpc.net/problem/2738}
+ */
+public class Problem2738 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,26 +20,21 @@ public class Main {
         int[][] A = new int[N][M];
 
 
-        for (int i = 0; i < N; i++) {
+        for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for(int j=0; j<M; j++){
                 A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
+        for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for(int j=0; j<M; j++){
                 sb.append(A[i][j] + Integer.parseInt(st.nextToken())).append(" ");
             }
             sb.append("\n");
         }
         System.out.println(sb);
-
     }
-
 }
-
-
-
