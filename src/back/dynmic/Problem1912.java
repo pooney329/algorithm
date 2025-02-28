ef94df6a0551d1bb97c84d376256117d718894d3
pooney329 <blue672@naver.com>
2025-02-28 09:20:01 +0900

    Problem1912 임시-2

--- src/back/dynmic/Problem1912.java
+++ src/back/dynmic/Problem1912.java
@@ -23,35 +23,42 @@ import java.util.StringTokenizer;
 -1
  */
 public class Problem1912 {
-    static int[] numbers = new int[100];
+    static long[] numbers = new long[100];
+    static long[][] cache = new long[100][100];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
-        numbers = new int[count];
+        numbers = new long[count];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < count; i++) {
             numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i=0; i<numbers.length; i++){
-            recursive(i);
-        }
 
+        recursive();
         System.out.println(MAX);
     }
 
 
-    static int MAX = Integer.MIN_VALUE;
+    static long MAX = Long.MIN_VALUE;
 
-    public static void recursive(int index) {
-
-        int temp = 0;
-        for (int i = index; i < numbers.length; i++) {
-            temp += numbers[i];
-            MAX = Math.max(MAX, temp);
+    public static void recursive() {
+        cache[0][0] = numbers[0];
+        MAX = numbers[0];
+        for(int i = 0; i < numbers.length - 1; i++) {
+            long sum = numbers[i];
+            for(int j = i+1; j < numbers.length; j++) {
+                sum += numbers[j];
+                if(cache[i][j]==0){
+                    cache[i][j]=sum;
+                    cache[i+1][j] = sum - cache[i][j];
+                }
+                MAX = Math.max(MAX, cache[i][j]);
+            }
         }
+
     }
 
 }
