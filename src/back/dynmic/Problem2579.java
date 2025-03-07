33346ca1a42b33b613a15b1d44c007c049ed2f84
pooney329 <blue672@naver.com>
2025-03-07 09:27:16 +0900

    Problem2579 임시-2

--- src/back/dynmic/Problem2579.java
+++ src/back/dynmic/Problem2579.java
@@ -5,6 +5,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
+/**
+ * 6
+ *
+ * 6
+ * 10
+ * 20
+ * 15
+ * 25
+ * 10
+ * 20
+ *
+ *
+ *
+ * == 75
+ *
+ *
+ * 부터 첫 번째, 두 번째, 네 번째, 여섯 번째 계단을 밟아 도착점에 도달하면 총 점수는 10 + 20 + 25 + 20 = 75점이 된다.
+ */
 public class Problem2579 {
 
     static int[] numberArray;
@@ -26,19 +45,20 @@ public class Problem2579 {
 
         dp[0] = numberArray[0];
         dp[1] = numberArray[1];
-        dp[2] = Math.max(dp[0],dp[1]) + numberArray[2];
+        dp[2] = Math.max(dp[0], dp[1]);
+        dp[3] = Math.max(dp[0] + numberArray[2] , dp[0] + numberArray[1]) + numberArray[3];
 
         System.out.println(recursive(count - 1));
     }
 
 
     public static int recursive(int n){
-        if(n <= 0 || visited[n]) return 0;
-        if(dp[n] == 0){
-            visited[n] = true;
-            dp[n] = Math.max(recursive(n-3) +  recursive(n-1), recursive(n-2 ) + recursive(n-3)) + numberArray[n];
-        }
-        return dp[n];
+        System.out.println("n : " + n);
+        if(n < 0) return 0;
+       if(dp[n] == 0) {
+           dp[n] = Math.max(recursive(n-3) + numberArray[n-1] , recursive(n-3) + numberArray[n-2]) + numberArray[n];
+       }
+       return dp[n];
     }
 
 }
