59431f511781b3aa60a8329fb9e6b13f175d6fb2
janghyeonwoo <blue672@naver.com>
2025-04-12 21:38:17 +0900

    Solution120802 add

---
+++ src/progrmmers/initiate/Solution120802.java
@@ -0,0 +1,18 @@
+package progrmmers.initiate;
+
+/**
+ * {@link https://school.programmers.co.kr/learn/courses/30/lessons/120802}
+ */
+public class Solution120802 {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        System.out.println(new Solution120802().solution(num1, num2));
+    }
+
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+
+
+}
