535fead2a1ddebf0309d187d5416af8a90d03336
janghyeonwoo <blue672@naver.com>
2025-04-15 22:52:16 +0900

    Solution43164 add

--- src/progrmmers/search/Solution43164.java
+++ src/progrmmers/search/Solution43164.java
@@ -1,65 +1,40 @@
 package progrmmers.search;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
- * 주어진 항공권을 모두 이용하여 여행경로를 짜려고 합니다. 항상 "ICN" 공항에서 출발합니다.
- * <p>
- * 항공권 정보가 담긴 2차원 배열 tickets가 매개변수로 주어질 때, 방문하는 공항 경로를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
- * <p>
- * 제한사항
- * 모든 공항은 알파벳 대문자 3글자로 이루어집니다.
- * 주어진 공항 수는 3개 이상 10,000개 이하입니다.
- * tickets의 각 행 [a, b]는 a 공항에서 b 공항으로 가는 항공권이 있다는 의미입니다.
- * 주어진 항공권은 모두 사용해야 합니다.
- * 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 합니다.
- * 모든 도시를 방문할 수 없는 경우는 주어지지 않습니다.
- * 입출력 예
- * tickets	return
- * [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]	["ICN", "JFK", "HND", "IAD"]
- * [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]	["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"]
- * 입출력 예 설명
- * 예제 #1
- * <p>
- * ["ICN", "JFK", "HND", "IAD"] 순으로 방문할 수 있습니다.
- * <p>
- * 예제 #2
- * <p>
- * ["ICN", "SFO", "ATL", "ICN", "ATL", "SFO"] 순으로 방문할 수도 있지만 ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"] 가 알파벳 순으로 앞섭니다.
+ * {@link https://school.programmers.co.kr/learn/courses/30/lessons/43164}
  */
 public class Solution43164 {
 
+
     public static void main(String[] args) {
-        String [][] tickets = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
+//        String [][] tickets = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
+        String [][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
         System.out.println(Arrays.toString(new Solution43164().solution(tickets)));;
     }
+    static List<String> airportArray = new ArrayList<>();
     public String[] solution(String[][] tickets) {
         String[] answer = {};
-
         boolean visited [] = new boolean [tickets.length];
-        Queue<String> queue = new LinkedList<>();
+        dfs(tickets, visited, "ICN", 0, "ICN");
 
+        List<String> collect = airportArray.stream().sorted().collect(Collectors.toList());
+        return collect.get(0).split(",");
+    }
+
+    public void dfs(String[][] tickets, boolean[] visited, String keyword, int depth, String str){
+        if(depth == tickets.length){
+            airportArray.add(str);
+            return;
+        }
         for(int i=0; i<tickets.length; i++){
-            if(tickets[i][0] == "ICN"){
-                queue.add(tickets[i][1]);
+            if(!visited[i] && tickets[i][0].equals(keyword)){
                 visited[i] = true;
+                dfs(tickets, visited, tickets[i][1], depth + 1, str + "," + tickets[i][1]);;
+                visited[i] = false;
             }
         }
-
-
-        while(!queue.isEmpty()){
-            String target = queue.poll();
-
-            for(int i=0; i<tickets.length; i++){
-                if(!visited[i]){
-
-                }
-            }
-
-        }
-
-        return answer;
     }
 }
