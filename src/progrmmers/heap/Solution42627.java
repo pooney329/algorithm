ef8f7f771266867121e6af041324acd104f9dc2d
pooney329 <blue672@naver.com>
2025-04-28 09:53:20 +0900

    Solution42627 임시

--- src/progrmmers/heap/Solution42627.java
+++ src/progrmmers/heap/Solution42627.java
@@ -1,19 +1,97 @@
 package progrmmers.heap;
 
+import java.util.PriorityQueue;
+
 /**
- * 하드디스크는 한 번에 하나의 작업만 수행할 수 있습니다. 디스크 컨트롤러를 구현하는 방법은 여러 가지가 있습니다. 이 문제에서는 우선순위 디스크 컨트롤러라는 가상의 장치를 이용한다고 가정합니다. 우선순위 디스크 컨트롤러는 다음과 같이 동작합니다.
- *
- * 어떤 작업 요청이 들어왔을 때 작업의 번호, 작업의 요청 시각, 작업의 소요 시간을 저장해 두는 대기 큐가 있습니다. 처음에 이 큐는 비어있습니다.
- * 디스크 컨트롤러는 하드디스크가 작업을 하고 있지 않고 대기 큐가 비어있지 않다면 가장 우선순위가 높은 작업을 대기 큐에서 꺼내서 하드디스크에 그 작업을 시킵니다. 이때, 작업의 소요시간이 짧은 것, 작업의 요청 시각이 빠른 것, 작업의 번호가 작은 것 순으로 우선순위가 높습니다.
- * 하드디스크는 작업을 한 번 시작하면 작업을 마칠 때까지 그 작업만 수행합니다.
- * 하드디스크가 어떤 작업을 마치는 시점과 다른 작업 요청이 들어오는 시점이 겹친다면 하드디스크가 작업을 마치자마자 디스크 컨트롤러는 요청이 들어온 작업을 대기 큐에 저장한 뒤 우선순위가 높은 작업을 대기 큐에서 꺼내서 하드디스크에 그 작업을 시킵니다. 또, 하드디스크가 어떤 작업을 마치는 시점에 다른 작업이 들어오지 않더라도 그 작업을 마치자마자 또 다른 작업을 시작할 수 있습니다. 이 과정에서 걸리는 시간은 없다고 가정합니다.
- * 예를 들어
- *
- * - 0ms 시점에 3ms가 소요되는 0번 작업 요청
- * - 1ms 시점에 9ms가 소요되는 1번 작업 요청
- * - 3ms 시점에 5ms가 소요되는 2번 작업 요청
- * 와 같은 요청이 들어왔습니다. 이를 그림으로 표현하면 다음과 같습
- *
+ * 작업의 소요시간이 짧은 것, 작업의 요청 시각이 빠른 것, 작업의 번호가 작은 것 순으로 우선순위가 높
  */
+
 public class Solution42627 {
+
+    public static void main(String[] args) {
+        Solution42627 solution42627 = new Solution42627();
+        int[][] jobs = new int[][]{{0, 3}, {1, 9}, {3, 5}};
+        System.out.println(solution42627.solution(jobs));
+    }
+
+
+    public int solution(int[][] jobs) {
+        int answer = 0;
+
+        PriorityQueue<Job> jobPriorityQueue = new PriorityQueue<>((job1, job2) -> {
+            if (job1.getJobTime() != job2.getJobTime()) {
+                return job1.getJobTime() - job2.getJobTime();
+            } else if (job1.requestTime != job2.requestTime) {
+                return job1.getJobTime() - job2.getJobTime();
+            } else {
+                return job1.getNo() - job2.getNo();
+            }
+        });
+
+        for (int i = 0; i < jobs.length; i++) {
+            jobPriorityQueue.add(new Job(i, jobs[i][0], jobs[i][1]));
+        }
+
+        int i = 0;
+        while (!jobPriorityQueue.isEmpty()) {
+            Job waitJob = jobPriorityQueue.peek();
+
+            //완료 된 잡
+            if (waitJob.getRequestTime() <= i && waitJob.getJobTime() == 0) {
+                waitJob.setFinishTime(i - waitJob.requestTime);
+                answer += waitJob.getFinishTime();
+                jobPriorityQueue.poll();
+                System.out.println(waitJob.getFinishTime());
+                continue;
+            }
+
+            //진행중인 잡
+            waitJob.minusJobTime();
+            i++;
+
+        }
+
+
+        return answer / jobs.length;
+    }
+
+
+    public static class Job {
+        private int no;
+        private int requestTime;
+        private int jobTime;
+
+        private int finishTime;
+
+        public Job(int no, int requestTime, int jobTime) {
+            this.no = no;
+            this.requestTime = requestTime;
+            this.jobTime = jobTime;
+        }
+
+        public int getNo() {
+            return no;
+        }
+
+        public int getRequestTime() {
+            return requestTime;
+        }
+
+        public int getJobTime() {
+            return jobTime;
+        }
+
+        public void setFinishTime(int finishTime) {
+            this.finishTime = finishTime;
+        }
+
+        public int getFinishTime() {
+            return finishTime;
+        }
+
+        public void minusJobTime() {
+            this.jobTime = this.jobTime - 1;
+        }
+
+    }
 }
