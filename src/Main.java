ac391423fbc1f4c4646976d610b6c3a2aeadc35b
janghyeonwoo <blue672@naver.com>
2025-05-11 22:23:02 +0900

    Solution43162 재풀이

--- src/Main.java
+++ src/Main.java
@@ -13,50 +13,39 @@ public class Main {
     static int[][] DP;
 
     public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        int n = 3;
+//        int[][] computers = {
+//                {1, 1, 0}, {1, 1, 0}, {0, 0, 1}
+//        };
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        Cost = new int[N][3];
-        DP = new int[N][3];
-
-        StringTokenizer st;
-        for(int i = 0; i < N; i++) {
-
-            st = new StringTokenizer(br.readLine(), " ");
-
-            Cost[i][Red] = Integer.parseInt(st.nextToken());
-            Cost[i][Green] = Integer.parseInt(st.nextToken());
-            Cost[i][Blue] = Integer.parseInt(st.nextToken());
-        }
-
-        // DP의 첫번째 값(집)은 각 색상비용의 첫번째 값으로 초기화
-        DP[0][Red] = Cost[0][Red];
-        DP[0][Green] = Cost[0][Green];
-        DP[0][Blue] = Cost[0][Blue];
-
-
-        System.out.println(Math.min(Paint_cost(N- 1, Red), Math.min(Paint_cost(N - 1, Green), Paint_cost(N - 1, Blue))));
+        int[][] computers = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+        System.out.println(main.solution(n, computers));
     }
 
-    static int Paint_cost(int N, int color) {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
 
-        // 만약 탐색하지 않은 배열이라면
-        if(DP[N][color] == 0) {
+        boolean[] visited = new boolean[n];
 
-            // color의 색에 따라 이전 집의 서로 다른 색을 재귀호출하여 최솟값과 현재 집의 비용을 더해서 DP에 저장한다
-            if(color == Red) {
-                DP[N][Red] = Math.min(Paint_cost(N - 1, Green), Paint_cost(N - 1, Blue)) + Cost[N][Red];
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                search(computers, i, visited);
+                answer++;
             }
-            else if(color == Green) {
-                DP[N][Green] = Math.min(Paint_cost(N - 1, Red), Paint_cost(N - 1, Blue)) + Cost[N][Green];
-            }
-            else {
-                DP[N][Blue] = Math.min(Paint_cost(N - 1, Red), Paint_cost(N - 1, Green)) + Cost[N][Blue];
-            }
-
         }
-
-        return DP[N][color];
+        return answer;
     }
+
+    public void search(int[][] computers, int index, boolean[] visited){
+        for(int i=0; i<computers[index].length; i++){
+            int v = computers[index][i];
+            if(!visited[i] && v == 1){
+                visited[i] = true;
+                search(computers, i, visited);
+            }
+        }
+    }
+
 }
