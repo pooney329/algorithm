6c1bda950fc48e9cc511e6a9e5c6e9a5dac8f1f7
janghyeonwoo <blue672@naver.com>
2025-05-14 21:13:22 +0900

    Solution120898 add

---
+++ src/progrmmers/initiate/Solution120898.java
@@ -0,0 +1,16 @@
+package progrmmers.initiate;
+
+/**
+ * {@link https://school.programmers.co.kr/learn/courses/30/lessons/120898}
+ */
+public class Solution120898 {
+
+    public static void main(String[] args) {
+        Solution120898 Solution120898 = new Solution120898();
+        System.out.println(Solution120898.solution("have a nice day"));
+    }
+
+    public int solution(String message) {
+        return message.length() * 2;
+    }
+}
