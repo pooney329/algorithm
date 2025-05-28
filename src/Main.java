a133526cfc739216606c42ce596196276012ff37
pooney329 <blue672@naver.com>
2025-05-29 08:28:40 +0900

    Solution42628 재풀이

--- src/Main.java
+++ src/Main.java
@@ -14,32 +14,71 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
-        int[] scoville = {1, 2, 3, 9, 10, 12};
-        int k = 7;
 
-
-        System.out.println((main.solution(scoville,k)));
+        String [] op = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
+        System.out.println(Arrays.toString(main.solution(op)));
     }
 
-    public int solution(int[] scoville, int K) {
+    public int[] solution(String[] operations) {
+        int[] answer = {};
 
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.naturalOrder());
-        for(int sco : scoville){
-            priorityQueue.add(sco);
+        //두개이 우선순위 큐를 준비
+        PriorityQueue<Integer> orderQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> reverseQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(String op : operations) {
+            String[] splitOperation = op.split(" ");
+            String command = splitOperation[0];
+            Integer number = Integer.parseInt(splitOperation[1]);
+
+            if(command.equals("I")){
+                orderQueue.add(number);
+                reverseQueue.add(number);
+            }
+            else if(command.equals("D")){
+                if(number == 1) {
+                    reverseQueue.poll();
+                    orderQueue.clear();;
+                    orderQueue.addAll(reverseQueue);
+                }
+                else {
+                    orderQueue.poll();
+                    reverseQueue.clear();;
+                    reverseQueue.addAll(orderQueue);
+                }
+            }
         }
 
-
-        int count = 0;
-        while(!priorityQueue.isEmpty()){
-            Integer target = priorityQueue.poll();
-            if(target >= K) break;
-            if(priorityQueue.isEmpty()) return -1;
-            int mix = target + (priorityQueue.poll() * 2);
-            priorityQueue.add(mix);
-            count++;
+        if(orderQueue.size() <= 1 || reverseQueue.size() <= 1){
+            return new int[]{0,0};
         }
 
-        return count;
+        return new int[]{reverseQueue.poll(), orderQueue.poll()};
+    }
+
+
+
+    public int[] solution(String[] operations) {
+        Queue<Integer> minpq = new PriorityQueue<>();
+        Queue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (String operation : operations) {
+            if (operation.startsWith("I ")) {
+                int n = Integer.parseInt(operation.substring(2));
+                minpq.offer(n);
+                maxpq.offer(n);
+            } else if (!minpq.isEmpty() && operation.equals("D -1")) {
+                maxpq.remove(minpq.poll());
+            } else if (!maxpq.isEmpty() && operation.equals("D 1")) {
+                minpq.remove(maxpq.poll());
+            }
+        }
+
+        if (minpq.isEmpty() && maxpq.isEmpty()) {
+            return new int[]{0, 0};
+        }
+
+        return new int[]{maxpq.poll(), minpq.poll()};
     }
 
 
--- src/progrmmers/heap/Solution42628.java
+++ src/progrmmers/heap/Solution42628.java
@@ -2,6 +2,7 @@ package progrmmers.heap;
 
 import java.util.*;
 
+
 /**
  * {@link https://school.programmers.co.kr/learn/courses/30/lessons/42628}
  */
@@ -43,5 +44,43 @@ public class Solution42628 {
         }
 
         return new int[]{list.get(list.size() - 1), list.get(0)};
+    }
+
+
+    public int[] solution2(String[] operations) {
+        int[] answer = {};
+
+        //두개이 우선순위 큐를 준비
+        PriorityQueue<Integer> orderQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> reverseQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(String op : operations) {
+            String[] splitOperation = op.split(" ");
+            String command = splitOperation[0];
+            Integer number = Integer.parseInt(splitOperation[1]);
+
+            if(command.equals("I")){
+                orderQueue.add(number);
+                reverseQueue.add(number);
+            }
+            else if(command.equals("D")){
+                if(number == 1) {
+                    reverseQueue.poll();
+                    orderQueue.clear();;
+                    orderQueue.addAll(reverseQueue);
+                }
+                else {
+                    orderQueue.poll();
+                    reverseQueue.clear();;
+                    reverseQueue.addAll(orderQueue);
+                }
+            }
+        }
+
+        if(orderQueue.size() <= 1 || reverseQueue.size() <= 1){
+            return new int[]{0,0};
+        }
+
+        return new int[]{reverseQueue.poll(), orderQueue.poll()};
     }
 }
