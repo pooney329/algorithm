47ab02d3f74032e2a7a6b5913351fb6509748819
janghyeonwoo <blue672@naver.com>
2025-07-08 22:20:28 +0900

    Solution43163_2 add

--- src/progrmmers/greedy/Solution42860.java
+++ src/progrmmers/greedy/Solution42860.java
@@ -3,33 +3,58 @@ package progrmmers.greedy;
 public class Solution42860 {
     public static void main(String[] args) {
         char a = 'A';
-        char b = 'B';
+        char b = 'Y';
 
+        Math.min((int) a + 25 - (int) b + 1, (int) b - (int) a);
+        System.out.println();
+        System.out.println((int) 'A');
+        System.out.println((int) 'Z');
         System.out.println(Math.abs((int) a- (int)b));
 
         Solution42860 Solution42860 = new Solution42860();
         System.out.println(Solution42860.solution("JEROEN"));
     }
 
-    public static int[] function = new int[]{1,2,3,4};
     public int solution(String name) {
         int answer = 0;
-        return answer;
-    }
 
-    public void recursive(String str, String target, int cursorIndex, int count) {
-        for(int i=0; i<str.length(); i++){
-            if(str.charAt(i) != target.charAt(i)){
-                if(i == 0){
-                    int cursorCount = Math.abs((int) str.charAt(i)- (int) target.charAt(i));
-                    recursive(str.substring(1), target.substring(1), cursorIndex, count + cursorCount);
-                }
+        String target = "A".repeat(name.length());
+        int rightCount = 0;
+        for(int i=0; i<name.length(); i++) {
+            if(name.charAt(i) != target.charAt(i)){
+                rightCount += minCount(name.charAt(i), target.charAt(i));
             }
-            else{
-
+            if(target == name) {
+                rightCount += i;
+                break;
             }
         }
 
-
+        int leftCount = 0;
+        for(int i=0; i<name.length(); i++) {
+            if(name.charAt(i) != target.charAt(i)){
+                rightCount += minCount(name.charAt(i), target.charAt(i));
+            }
+            if(target == name) {
+                rightCount += i;
+                break;
+            }
+        }
+        return rightCount;
     }
+
+    public int minCount(char a , char b) {
+        return Math.min((int) a + 25 - (int) b + 1, (int) b - (int) a);
+    }
+
+//    public void recursive(String name, String target, int cursorIdx, int count) {
+//        if(name.equals(target)) {
+//            return;
+//        }
+//        recursive(name, target, cursorIdx + 1, count);
+//        recursive(name, target, name.length() - cursorIdx - 1, count);
+//
+//    }
+
+
 }
