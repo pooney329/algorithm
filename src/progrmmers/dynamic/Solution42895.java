8e89e93e15dbce42402391ff591d1d6725174c7a
janghyeonwoo <blue672@naver.com>
2025-07-17 22:46:45 +0900

    Solution42895 add

--- src/progrmmers/dynamic/Solution42895.java
+++ src/progrmmers/dynamic/Solution42895.java
@@ -1,39 +1,52 @@
 package progrmmers.dynamic;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * {@link https://school.programmers.co.kr/learn/courses/30/lessons/42895}
+ */
 public class Solution42895 {
 
     public static void main(String[] args) {
-        System.out.println(solution(5, 12));
+        Solution42895 Solution42895 = new Solution42895();
+        System.out.println(Solution42895.solution(5, 12));
+        ;
     }
 
-
-    public static int MIN = 9;
-    public static String [] operation = new String []{"+","-","*", "/","++"};
-    public static int solution(int N, int number) {
+    public int solution(int N, int number) {
         int answer = 0;
 
-        int minValue = recursive(N, 0, 0, number, 0);
-        return minValue <= 8 ? minValue : -1;
-    }
-
-
-    public static int recursive(int number , int sum, int index,  int resultNumber , int count) {
-        if(sum == resultNumber || count > 8){
-            return count;
-        }
-
-        for(int i=index; i<operation.length; i++){
-            String op = operation[i];
-            switch (op) {
-                case "+" -> sum += number;
-                case "*" -> sum *= number;
-                case "-" -> sum -= number;
-                case "/" -> sum /= number;
-                case "++" -> sum = Integer.parseInt(String.valueOf(sum) + number);
+        Set<Integer>[] dp = new HashSet[10];
+        int concatNumber = 0;
+        int cnt = 1;
+        for (int i = 1; i <= 8; i++) {
+            dp[i] = new HashSet<>();
+            concatNumber = concatNumber + N * cnt;
+            dp[i].add(concatNumber);
+            cnt *= 10;
+            System.out.println("i : " + i);
+            for (int j = 1; j < i; j++) {
+                for (Integer num : dp[j]) {
+                    for(Integer v : dp[i - j]){
+                        dp[i].add(num + v);
+                        dp[i].add(num - v);
+                        dp[i].add(num * v);
+                        if(v != 0){
+                            dp[i].add(num / v);
+                        }
+                    }
+                }
             }
-            int cal = recursive(number, sum, index, resultNumber, count + 1);
-            MIN = Math.min(MIN, cal);
+            if(dp[i].contains(number)){
+                answer = i;
+                break;
+            }
         }
-        return MIN;
+
+
+        return answer == 0 ? -1 : answer;
     }
+
+
 }
