b7f60deda9fb0399d47876e14365658a1e1afdf9
janghyeonwoo <blue672@naver.com>
2025-08-02 18:58:42 +0900

    Solution42587 add

---
+++ src/progrmmers/stack/Solution42587.java
@@ -0,0 +1,73 @@
+package progrmmers.stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * {@link https://school.programmers.co.kr/learn/courses/30/lessons/42587/solution_groups?language=java}
+ */
+public class Solution42587 {
+    public static void main(String[] args) {
+        Solution42587 Solution42587 = new Solution42587();
+        int[] priorities = {1, 1, 9, 1, 1, 1};
+        int location = 0;
+        System.out.println(Solution42587.solution(priorities,location));
+    }
+
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<Program> priorityQueue = new LinkedList<>();
+        for(int i = 0; i < priorities.length; i++){
+            priorityQueue.add(new Program(i, priorities[i]));
+        }
+
+
+        while(!priorityQueue.isEmpty()){
+            answer++;
+            Program pro = priorityQueue.poll();
+            int max = pro.priority;
+            for(Program existProgram : priorityQueue) {
+                max = Math.max(max, existProgram.priority);
+            }
+            if(pro.priority < max){
+                priorityQueue.add(pro);
+                while(!priorityQueue.isEmpty()){
+                    Program poll = priorityQueue.poll();
+                    if(poll.priority != max) {
+                        priorityQueue.add(poll);
+                        continue;
+                    }
+                    if(poll.index == location) {
+                        return answer;
+                    }
+                    break;
+                }
+            }
+            else {
+                if(pro.index == location) {
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static class Program {
+        int index;
+        int priority;
+
+        public Program(int index, int priority) {
+            this.index = index;
+            this.priority = priority;
+        }
+
+        @Override
+        public String toString() {
+            return "Program{" +
+                    "index=" + index +
+                    ", priority=" + priority +
+                    '}';
+        }
+    }
+
+}
