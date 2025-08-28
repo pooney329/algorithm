8a31ba6b782240c08ca1b3221bbe6a114e54ee95
janghyeonwoo <blue672@naver.com>
2025-08-28 22:28:15 +0900

    Problem11659 add

---
+++ src/back/recursive/Problem11659.java
@@ -0,0 +1,31 @@
+package back.recursive;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * {@link https://www.acmicpc.net/problem/11659}
+ */
+public class Problem11659 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] dp = new int[N+1];
+        for(int i=1; i<=N; i++) {
+            dp[i] = dp[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            System.out.println(dp[end] - dp[start - 1]);
+        }
+    }
+}
